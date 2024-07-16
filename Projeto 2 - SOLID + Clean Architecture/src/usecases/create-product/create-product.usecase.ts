6e0f92ffaf024baa119f07fcd3eeb598b273c495
Bergkley <bergkley@gmail.com>
2024-07-15 22:40:33 -0300

    feat: Projeto 2 - SOLID + Clean Architecture

--- Projeto 2 - SOLID + Clean Architecture/src/usecases/create-product/create-product.usecase.ts
+++ Projeto 2 - SOLID + Clean Architecture/src/usecases/create-product/create-product.usecase.ts
@@ -1,43 +1,42 @@
 import { Product } from "../../domain/product/entity/product";
 import { ProductGateway } from "../../domain/product/gateway/product.gateway";
-import { Usecase } from "../usecase"
+import { Usecase } from "../usecase";
 
 export type CreateProductInputDto = {
-    name: string;
-    price:number;
-}
+  name: string;
+  price: number;
+};
 
 export type CreateProductOutputDto = {
-    id:string;
-}
+  id: string;
+};
 
+export class CreateProductUsecase
+  implements Usecase<CreateProductInputDto, CreateProductOutputDto>
+{
+  private constructor(private readonly productGateway: ProductGateway) {}
 
+  public static create(productGateway: ProductGateway) {
+    return new CreateProductUsecase(productGateway);
+  }
 
-export class CreateProductUsecase implements Usecase <CreateProductInputDto, CreateProductOutputDto> {
+  public async execute({
+    name,
+    price,
+  }: CreateProductInputDto): Promise<CreateProductOutputDto> {
+    const aProduct = Product.create(name, price);
 
-    private constructor(private readonly productGateway: ProductGateway) {}
+    await this.productGateway.save(aProduct);
 
-    public static create(productGateway: ProductGateway){
-        return new CreateProductUsecase(productGateway)
-    }
+    const output = this.presentOutput(aProduct);
+    return output;
+  }
 
-    public async execute({name,price,}:CreateProductInputDto): Promise<CreateProductOutputDto>{
-        const aProduct = Product.create(name, price)
+  private presentOutput(product: Product): CreateProductOutputDto {
+    const output: CreateProductOutputDto = {
+      id: product.id,
+    };
 
-        await this.productGateway.save(aProduct)
-
-        const output = this.presentOutput(aProduct)
-        return output
-    }
-
-    private presentOutput(product: Product): CreateProductOutputDto {
-        const output: CreateProductOutputDto = {
-            id: product.id,
-        }
-
-        return output
-    }
-
-    
-    
+    return output;
+  }
 }
