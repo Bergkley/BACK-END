d023b8516934c6920a21c6639358e9299343cbe0
Bergkley <bergkley@gmail.com>
2024-07-18 21:38:16 -0300

    feat: refatoração

--- Projeto 1 - Sistema de autenticação ( simples)/src/routes/api.ts
+++ Projeto 1 - Sistema de autenticação ( simples)/src/routes/api.ts
@@ -1,13 +1,10 @@
-import { Router } from 'express';
+import { Router } from "express";
 
-import * as ApiController from '../controllers/apiController';
+import * as ApiController from "../controllers/apiController";
 
 const router = Router();
 
-router.post('/register', ApiController.register);
-router.post('/login', ApiController.login);
-
-
-
+router.post("/register", ApiController.register);
+router.post("/login", ApiController.login);
 
 export default router;
