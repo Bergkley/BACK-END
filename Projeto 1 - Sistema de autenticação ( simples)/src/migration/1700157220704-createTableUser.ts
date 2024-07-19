0a36f9a7b02b78576f85ccf3296ab9d097a86b61
Bergkley <bergkley@gmail.com>
2024-07-18 21:38:27 -0300

    feat: refatoração

--- Projeto 1 - Sistema de autenticação ( simples)/src/migration/1700157220704-createTableUser.ts
+++ Projeto 1 - Sistema de autenticação ( simples)/src/migration/1700157220704-createTableUser.ts
@@ -1,13 +1,13 @@
-import { MigrationInterface, QueryRunner } from "typeorm"
+import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class CreateTableUser1700157220704 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL,"email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_3333333333333333" PRIMARY KEY ("id"))`
+    );
+  }
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "user" ("id" SERIAL NOT NULL, "name" character varying NOT NULL,"email" character varying NOT NULL, "password" character varying NOT NULL, CONSTRAINT "PK_3333333333333333" PRIMARY KEY ("id"))`);
-    }
-
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "user"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "user"`);
+  }
 }
